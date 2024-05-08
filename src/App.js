60d60064f03ad9bc08fae324d8eec07613780fa6
JJay1010 <1679ej@g.skku.edu>
2024-05-08 20:39:06 +0900

    검색 결과에서 앨범 상세까지 연결

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Profile from "./pages/profile/Profile";
 import EditProfile from "./pages/editProfile/EditProfile";
 import UserContextProvider from "./context/UserContext";
 import Search from "./pages/search/Search";
+import SearchResults from "./pages/search/SearchResults";
 import AlbumDetail from "./pages/albumDetail/AlbumDetail";
 import ReviewDetail from "./pages/reviewDetail/ReviewDetail";
 
@@ -21,8 +22,8 @@ function App() {
                         <Route path="/" element={<Home/>}/>
                         <Route path="/profile" element={<Profile/>}/>
                         <Route path="/editProfile" element={<EditProfile/>}/>
-                        <Route path="/search" element={<Search/>}/>
-                        <Route path="/albumDetail" element={<AlbumDetail/>}/>
+                        <Route path="/search" element={<SearchResults/>}/>
+                        <Route path="/albumDetail/:id" element={<AlbumDetail/>}/>
                         <Route path="/reviewDetail" element={<ReviewDetail/>}/>
                     </Routes>
                 </div>
--- src/pages/albumDetail/AlbumDetail.js
+++ src/pages/albumDetail/AlbumDetail.js
@@ -1,14 +1,17 @@
 import { createPortal } from "react-dom";
 import styles from "./AlbumDetail.module.css";
 import {useEffect, useState, useRef} from "react";
+import {useParams} from "react-router-dom";
 import AlbumReviewWrite from "../../components/albumReviewModal/AlbumReviewWrite";
 
 const AlbumDetail = () =>{
     const [reviewWriteModalOpen, setReviewWriteModalOpen] = useState(false);
     const reviewWriteModalBackground = useRef();
+    const { id } = useParams(); // id 파라미터 추출
 
     return (
         <>
+         <h1>Album ID: {id}</h1> {/* ID 출력 */}
         <div className={styles.reviewBtn}>
             <button className={styles.writeReview} onClick={() => setReviewWriteModalOpen(true)}>
             이 앨범 리뷰하기
