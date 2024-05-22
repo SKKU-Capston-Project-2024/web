b6b2a5978ec358c7247dd3bab2c25d8ccc95a798
seongyunlee <private.seongyun@gmail.com>
2024-05-22 19:02:57 +0900

    feat: make profile better

--- src/App.js
+++ src/App.js
@@ -7,11 +7,9 @@ import Profile from "./pages/profile/Profile";
 import EditProfile from "./pages/editProfile/EditProfile";
 import FollowUser from "./pages/followUser/followUser";
 import UserContextProvider from "./context/UserContext";
-import Search from "./pages/search/Search";
 import SearchResults from "./pages/search/SearchResults";
 import AlbumDetail from "./pages/albumDetail/AlbumDetail";
 import ReviewDetail from "./pages/reviewDetail/ReviewDetail";
-import StarRating3 from "./components/starRating2/StarRating3";
 import Playlist from "./pages/playlist/Playlist";
 import AddSong from "./pages/addSong/AddSong";
 import PlaylistAdd from "./pages/playlist/PlaylistAdd";
@@ -25,16 +23,16 @@ function App() {
                 <div className="frameWrapper">
                     <Routes>
                         <Route path="/" element={<Home/>}/>
-                        <Route path="/profile" element={<Profile/>}/>
+                        <Route path="/profile/:id" element={<Profile/>}/>
                         <Route path="/editProfile" element={<EditProfile/>}/>
                         <Route path="/search" element={<SearchResults/>}/>
                         <Route path="/albumDetail/:id" element={<AlbumDetail/>}/>
                         <Route path="/reviewDetail" element={<ReviewDetail/>}/>
                         <Route path="/reviewDetail/:id" element={<ReviewDetail/>}/>
                         <Route path="/followUser" element={<FollowUser/>}/>
-                        <Route path="/playlist" element={<Playlist/>} />
-                        <Route path="/addSong" element={<AddSong/>} />
-                        <Route path="/playlistadd" element={<PlaylistAdd/>} />
+                        <Route path="/playlist" element={<Playlist/>}/>
+                        <Route path="/addSong" element={<AddSong/>}/>
+                        <Route path="/playlistadd" element={<PlaylistAdd/>}/>
                     </Routes>
                 </div>
                 <FooterNav/>
